4703a5debbac80e617b0082f763b6e9ba23d47ad
B.C. Kim <klm03025@gmail.com>
2021-07-24 23:27:09 +0900

    Update constants.py

--- app/constants.py
+++ app/constants.py
@@ -1,7 +1,7 @@
 SQLALCHEMY_DATABASE_URI = ("mysql+pymysql://{USER}:{PASSWORD}@{ADDR}:{PORT}/{NAME}?charset=utf8")
 SQLALCHEMY_DATABASE_URI_FORMAT = SQLALCHEMY_DATABASE_URI.format(
-        USER="test",
-        PASSWORD="VSFe03025!",
+        USER="user_name",
+        PASSWORD="password",
         ADDR="localhost",
         PORT=3306,
         NAME="testdb"
